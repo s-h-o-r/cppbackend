830b6520f8429fc5c00b293a1a8426e190b612a6
Shor <danilbug@mail.ru>
2024-09-06 20:16:21 +0300

    game_state solution v1

---
+++ sprint2/problems/game_state/solution/src/app.cpp
@@ -0,0 +1,92 @@
+#include "app.h"
+
+namespace app {
+
+using namespace std::literals;
+
+std::string GetMapError::what() const {
+    switch (reason) {
+        case GetMapErrorReason::mapNotFound:
+            return "Map not found"s;
+    }
+    throw std::runtime_error("Unknown GetMapError");
+}
+
+const model::Map* GetMapUseCase::GetMap(model::Map::Id map_id) const {
+    const model::Map* map = game_->FindMap(map_id);
+    if (map == nullptr) {
+        throw GetMapError{GetMapErrorReason::mapNotFound};
+    }
+    return map;
+}
+
+const model::Game::Maps& ListMapsUseCase::GetMapsList() const {
+    return game_->GetMaps();
+}
+
+std::string ListPlayersError::what() const {
+    switch (reason) {
+        case ListPlayersErrorReason::unknownToken:
+            return "Player token has not been found"s;
+    }
+    throw std::runtime_error("Unknown ListPlayersError");
+}
+
+const model::GameSession::IdToDogIndex& ListPlayersUseCase::GetPlayersList(std::string_view token) const {
+    const user::Player* player = tokens_->FindPlayerByToken(user::Token{std::string(token)});
+    if (player == nullptr) {
+        throw ListPlayersError{ListPlayersErrorReason::unknownToken};
+    }
+    return player->GetGameSession()->GetDogs();
+}
+
+
+std::string JoinGameError::what() const {
+    switch (reason) {
+        case JoinGameErrorReason::invalidMap:
+            return "Map not found"s;
+        case JoinGameErrorReason::invalidName:
+            return "Invalid name"s;
+    }
+    throw std::runtime_error("Unknown JoinGameError");
+}
+
+JoinGameResult JoinGameUseCase::JoinGame(const std::string& user_name, const std::string& map_id) {
+    if (user_name.empty()) {
+        throw JoinGameError{JoinGameErrorReason::invalidName};
+    }
+
+    const model::Map* map = game_->FindMap(model::Map::Id{map_id});
+
+    if (!map) {
+        throw JoinGameError{JoinGameErrorReason::invalidMap};
+    }
+
+    model::GameSession* session = game_->GetGameSession(model::Map::Id{map_id});
+    if (session == nullptr) {
+        session = &game_->StartGameSession(ioc_, map);
+    }
+
+    model::Dog* dog = session->AddDog(user_name);
+    user::Token player_token = tokens_->AddPlayer(&players_->Add(dog, session));
+
+    return {player_token, dog->GetId()};
+}
+
+const model::Game::Maps& Application::ListMaps() const {
+    return list_maps_use_case_.GetMapsList();
+}
+
+const model::Map* Application::FindMap(model::Map::Id map_id) const {
+    return get_map_use_case_.GetMap(map_id);
+}
+
+const model::GameSession::IdToDogIndex& Application::ListPlayers(std::string_view token) const {
+    return list_players_use_case_.GetPlayersList(token);
+}
+
+JoinGameResult Application::JoinGame(const std::string& user_name, const std::string& map_id) {
+    return join_game_use_case_.JoinGame(user_name, map_id);
+}
+
+} // namespace app
---
+++ sprint2/problems/game_state/solution/src/request_handler.h
@@ -0,0 +1,431 @@
+#pragma once
+
+#include <boost/json.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+
+#include "app.h"
+#include "http_server.h"
+#include "model.h"
+#include "player.h"
+
+#include <algorithm>
+#include <cassert>
+#include <filesystem>
+#include <iomanip>
+#include <memory>
+#include <optional>
+#include <string_view>
+#include <unordered_map>
+
+namespace http_handler {
+
+namespace fs = std::filesystem;
+namespace net = boost::asio;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace json = boost::json;
+
+enum class Extention {
+    empty, unkown,
+    htm, html, css, txt, js, json, xml,
+    png, jpg, jpe, jpeg, gif, bmp, ico,
+    tiff, tif, svg, svgz, mp3
+};
+
+class Uri {
+public:
+    explicit Uri(std::string_view uri, const fs::path& base);
+
+    const fs::path& GetCanonicalUri() const;
+
+    Extention GetFileExtention() const;
+
+    bool IsSubPath(const fs::path& base) const;
+
+private:
+    fs::path uri_;
+    fs::path canonical_uri_;
+
+    std::string EncodeUri(std::string_view uri) const;
+};
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view APP_JSON = "application/json";
+    constexpr static std::string_view APP_XML = "application/xml";
+    constexpr static std::string_view APP_BINARY = "application/octet-stream";
+
+    constexpr static std::string_view TXT_HTML = "text/html";
+    constexpr static std::string_view TXT_CSS = "text/css";
+    constexpr static std::string_view TXT_PLAIN = "text/plain";
+    constexpr static std::string_view TXT_JS = "text/javascript";
+
+    constexpr static std::string_view IMG_PNG = "image/png";
+    constexpr static std::string_view IMG_JPEG = "image/jpeg";
+    constexpr static std::string_view IMG_GIF = "image/gif";
+    constexpr static std::string_view IMG_BMP = "image/bmp";
+    constexpr static std::string_view IMG_ICON = "image/vnd.microsoft.icon";
+    constexpr static std::string_view IMG_TIFF = "image/tiff";
+    constexpr static std::string_view IMG_SVG_XML = "image/svg+xml";
+
+    constexpr static std::string_view AUDIO_MPEG = "audio/mpeg";
+};
+
+template <typename Request, typename Response>
+void FillBasicInfo(Request& req, Response& response) {
+    response.version(req.version());
+    response.keep_alive(req.keep_alive());
+}
+
+std::string_view GetMimeType(Extention extention);
+std::string ParseMapToJson(const model::Map* map);
+
+using StringResponse = http::response<http::string_body>;
+using FileResponse = http::response<http::file_body>;
+
+class ApiRequestHandler : public std::enable_shared_from_this<ApiRequestHandler> {
+public:
+    using Strand = net::strand<net::io_context::executor_type>;
+
+    explicit ApiRequestHandler(model::Game& game, net::io_context& ioc)
+    : app_(ioc, &game)
+    , ioc_(ioc)
+    , strand_{net::make_strand(ioc_)} {
+    }
+
+    ApiRequestHandler(const ApiRequestHandler&) = delete;
+    ApiRequestHandler& operator=(const ApiRequestHandler&) = delete;
+
+    template <typename Request, typename Send>
+    void operator() (Request&& req, Send&& send) {
+        net::dispatch(ioc_, [self = shared_from_this(), &req, &send] {
+            std::string_view target = req.target();
+            self->SendApiResponse(req, std::forward<Send>(send), target);
+        });
+    }
+
+private:
+    app::Application app_;
+    net::io_context& ioc_;
+    Strand strand_;
+
+    template <typename Request, typename Send>
+    void SendApiResponse(Request& req, Send&& send, std::string_view target) {
+        using namespace std::literals;
+
+        StringResponse response;
+        try {
+            FillBasicInfo(req, response);
+            
+            if (target.substr(0, 12) == "/api/v1/maps"sv) {
+                switch (req.method()) {
+                    case http::verb::get:
+                    case http::verb::head:
+                        ProcessApiMaps(response, target);
+                        break;
+                    default:
+                        MakeErrorApiResponse(response, ApiRequestHandler::ErrorCode::invalid_method_common, "Invalid method"sv);
+                        break;
+                }
+            } else if (target.substr(0, 20) == "/api/v1/game/players"sv) {
+                switch (req.method()) {
+                    case http::verb::get:
+                    case http::verb::head:
+                        ProcessApiPlayers(req, response);
+                        break;
+                    default:
+                        MakeErrorApiResponse(response, ApiRequestHandler::ErrorCode::invalid_method_players,
+                                             "Invalid method"sv);
+                        break;
+                }
+            } else if (target.substr(0, 17) == "/api/v1/game/join"sv) {
+                switch (req.method()) {
+                    case http::verb::post:
+                        net::dispatch(strand_, [self = shared_from_this(), &req, &response] {
+                            assert(self->strand_.running_in_this_thread());
+                            self->ProcessApiJoin(req, response);
+                        });
+                        break;
+                    default:
+                        MakeErrorApiResponse(response, ApiRequestHandler::ErrorCode::invalid_method_join,
+                                             "Only POST method is expected"sv);
+                        break;
+                }
+            } else if (target.substr(0, 18) == "/api/v1/game/state"sv) {
+                switch (req.method()) {
+                    case http::verb::get:
+                    case http::verb::head:
+                        ProcessApiGameState(req, response);
+                        break;
+                    default:
+                        MakeErrorApiResponse(response, ApiRequestHandler::ErrorCode::invalid_method_players,
+                                             "Invalid method"sv);
+                        break;
+                }
+            } else {
+                MakeErrorApiResponse(response, ApiRequestHandler::ErrorCode::bad_request, "Bad request"sv);
+            }
+            
+        } catch (...) {
+            MakeErrorApiResponse(response, ApiRequestHandler::ErrorCode::unknown, "Unknown send api response error"sv);
+        }
+        send(response);
+    }
+
+    void ProcessApiMaps(StringResponse& response, std::string_view target) const;
+
+    template <typename Request>
+    void ProcessApiPlayers(Request& request, StringResponse& response) const {
+        using namespace std::literals;
+
+        response.set(http::field::cache_control, "no-cache");
+
+        std::string_view token;
+
+        try {
+             token = GetRawTokenValue(request);
+        } catch (const ErrorCode ec) {
+            switch (ec) {
+                case ErrorCode::invalid_token:
+                    MakeErrorApiResponse(response, ErrorCode::invalid_token, "Authorization header is missing"sv);
+                    break;
+                default:
+                    MakeErrorApiResponse(response, ErrorCode::unknown, "Unknown error code while getting raw token"sv);
+                    break;
+            }
+            return;
+        }
+
+        try {
+            const auto& players = app_.ListPlayers(token);
+            json::object players_on_map_json;
+            for (const auto& [id, player] : players) {
+                players_on_map_json[std::to_string(*id)] = {{"name", player->GetName()}};
+            }
+
+            response.body() = json::serialize(json::value(std::move(players_on_map_json)));
+        } catch (const app::ListPlayersError& error) {
+            switch (error.reason) {
+                case app::ListPlayersErrorReason::unknownToken:
+                    MakeErrorApiResponse(response, ErrorCode::unknown_token, error.what());
+                    return;
+            }
+        }
+
+        response.set(http::field::content_type, ContentType::APP_JSON);
+        response.content_length(response.body().size());
+        response.result(http::status::ok);
+    }
+
+    template <typename Request>
+    void ProcessApiJoin(Request& request, StringResponse& response) {
+        using namespace std::literals;
+
+        response.set(http::field::cache_control, "no-cache");
+
+        boost::system::error_code ec;
+        json::value request_body = json::parse(request.body(), ec);
+        if (ec || !(request_body.if_object() && request_body.as_object().count("userName") && request_body.as_object().count("mapId"))) {
+            MakeErrorApiResponse(response, ErrorCode::invalid_argument, "Join game request parse error"sv);
+            return;
+        }
+
+        try {
+            std::string user_name = std::string(request_body.as_object().at("userName").as_string());
+            std::string map_id = std::string(request_body.as_object().at("mapId").as_string());
+            
+            auto join_result = app_.JoinGame(user_name, map_id);
+
+            json::value jv = {
+                {"authToken", *join_result.token},
+                {"playerId", *join_result.player_id}
+            };
+
+            response.body() = json::serialize(jv);
+
+        } catch (const app::JoinGameError& error) {
+            switch (error.reason) {
+                case app::JoinGameErrorReason::invalidMap:
+                    MakeErrorApiResponse(response, ErrorCode::map_not_found, error.what());
+                    return;
+                case app::JoinGameErrorReason::invalidName:
+                    MakeErrorApiResponse(response, ErrorCode::invalid_argument, error.what());
+                    return;
+            }
+        }
+
+        response.set(http::field::content_type, ContentType::APP_JSON);
+        response.content_length(response.body().size());
+        response.result(http::status::ok);
+    }
+
+    template <typename Request>
+    void ProcessApiGameState(Request& request, StringResponse& response) {
+        using namespace std::literals;
+
+        response.set(http::field::cache_control, "no-cache");
+
+        std::string_view token;
+
+        try {
+             token = GetRawTokenValue(request);
+        } catch (const ErrorCode ec) {
+            switch (ec) {
+                case ErrorCode::invalid_token:
+                    MakeErrorApiResponse(response, ErrorCode::invalid_token, "Authorization header is required"sv);
+                    break;
+                default:
+                    MakeErrorApiResponse(response, ErrorCode::unknown, "Unknown error code while getting raw token"sv);
+                    break;
+            }
+            return;
+        }
+
+        try {
+            const auto& players = app_.ListPlayers(token);
+            json::object players_on_map_json;
+            for (const auto& [id, player] : players) {
+                const model::DogPoint& pos = player->GetPosition();
+                const model::Speed& speed = player->GetSpeed();
+
+                players_on_map_json[std::to_string(*id)] = {
+                    {"pos", {pos.x, pos.y}},
+                    {"speed", {speed.s_x, speed.s_y}},
+                    {"dir", player->GetDirection()}
+                };
+            }
+
+            response.body() = json::serialize(json::value(std::move(players_on_map_json)));
+        } catch (const app::ListPlayersError& error) {
+            switch (error.reason) {
+                case app::ListPlayersErrorReason::unknownToken:
+                    MakeErrorApiResponse(response, ErrorCode::unknown_token, error.what());
+                    return;
+            }
+        }
+
+        response.set(http::field::content_type, ContentType::APP_JSON);
+        response.content_length(response.body().size());
+        response.result(http::status::ok);
+    }
+
+    enum class ErrorCode {
+        unknown,
+        map_not_found, invalid_method_common, invalid_method_join,
+        invalid_argument, bad_request, invalid_token, unknown_token,
+        invalid_method_players
+    };
+
+
+    template <typename Request>
+    std::string_view GetRawTokenValue(Request& request) const {
+        if (!request.base().count("Authorization")) {
+            throw ErrorCode::invalid_token;
+        }
+
+        auto auth_content = request.base().at("Authorization");
+        if (auth_content.size() < 39 || auth_content.size() > 39) { // size of Bearer + ' ' + token.size()
+            throw ErrorCode::invalid_token;
+        }
+
+        std::string_view token_prefix = auth_content.substr(0, 7);
+        std::string_view token = auth_content.substr(7); // Убираем Bearer перед токеном
+
+        if (std::size_t token_size = 32; token_prefix != "Bearer " || token.size() != token_size) {
+            throw ErrorCode::invalid_token;
+        }
+        return token;
+    }
+
+    void MakeErrorApiResponse(StringResponse& response, ApiRequestHandler::ErrorCode code,
+                              std::string_view message) const;
+};
+
+class StaticRequestHandler {
+public:
+    explicit StaticRequestHandler(std::filesystem::path&& static_files_path)
+    : static_files_path_(std::move(static_files_path)) {
+    }
+
+    StaticRequestHandler(const StaticRequestHandler&) = delete;
+    StaticRequestHandler& operator=(const StaticRequestHandler&) = delete;
+
+    template <typename Request, typename Send>
+    void operator() (Request&& req, Send&& send) {
+        try {
+            std::string_view target = req.target();
+            SendStaticFile(req, std::forward<Send>(send), target);
+        } catch (...) {
+            send(MakeErrorStaticFileResponse(http::status::unknown));
+        }
+    }
+
+private:
+    std::filesystem::path static_files_path_;
+
+    template <typename Request, typename Send>
+    void SendStaticFile(Request& req, Send&& send, std::string_view target) const {
+        FileResponse response;
+        FillBasicInfo(req, response);
+
+        http::status status{};
+
+        switch (req.method()) {
+            case http::verb::get:
+            case http::verb::head:
+                status = ProcessStaticFileTarget(response, target);
+                break;
+
+            default:
+                auto error_response = MakeErrorStaticFileResponse(http::status::method_not_allowed);
+                FillBasicInfo(req, error_response);
+                send(error_response);
+                return;
+        }
+
+        if (status != http::status::ok) {
+            auto error_response = MakeErrorStaticFileResponse(status);
+            FillBasicInfo(req, error_response);
+            send(error_response);
+        } else {
+            send(response);
+        }
+    }
+
+    http::status ProcessStaticFileTarget(FileResponse& response, std::string_view target) const;
+    StringResponse MakeErrorStaticFileResponse(http::status status) const;
+};
+
+class RequestHandler : public std::enable_shared_from_this<RequestHandler> {
+public:
+    explicit RequestHandler(model::Game& game, net::io_context& ioc, std::filesystem::path&& static_files_path)
+        : ioc_(ioc)
+        , api_handler_(std::make_shared<ApiRequestHandler>(game, ioc))
+        , static_handler_(std::move(fs::canonical(static_files_path))) {
+    }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        using namespace std::literals;
+
+        std::string_view target = req.target();
+        if (target.size() >= 4 && target.substr(0, 5) == "/api/"sv) {
+            net::dispatch(ioc_, [self = shared_from_this(), &req, &send] {
+                (*self->api_handler_)(std::forward<decltype(req)>(req), std::forward<Send>(send));
+            });
+        } else {
+            static_handler_(std::forward<decltype(req)>(req), std::forward<Send>(send));
+        }
+    }
+
+private:
+    net::io_context& ioc_;
+    std::shared_ptr<ApiRequestHandler> api_handler_;
+    StaticRequestHandler static_handler_;
+};
+
+}  // namespace http_handler
