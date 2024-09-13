c4636010affe0a85f263be6cdc0cc12bf2f02d9e
Shor <danilbug@mail.ru>
2024-09-13 19:54:45 +0300

    time_control solution v1

---
+++ sprint2/problems/time_control/solution/src/logger.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <boost/beast/http/field.hpp>
+#include <boost/beast/http/verb.hpp>
+#include <boost/json.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <string_view>
+
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+namespace http = boost::beast::http;
+
+namespace http_logger {
+
+class DurationMeasure {
+public:
+    DurationMeasure() = default;
+
+    auto Count() {
+        std::chrono::system_clock::time_point end_ts = std::chrono::system_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(end_ts - start_ts_).count();
+    };
+
+private:
+    std::chrono::system_clock::time_point start_ts_ = std::chrono::system_clock::now();
+};
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+BOOST_LOG_ATTRIBUTE_KEYWORD(log_data, "LogData", boost::json::value)
+
+void LogFormatter(logging::record_view const& rec, logging::formatting_ostream& strm);
+
+template <typename Formatter>
+void InitBoostLogFilter(Formatter&& formatter) {
+    logging::add_common_attributes();
+    
+    logging::add_console_log(
+        std::cout,
+        keywords::format = &formatter,
+        keywords::auto_flush = true
+    );
+}
+
+template <typename Request>
+void LogRequest(std::string_view client_ip, Request& request) {
+    boost::json::value data = {
+        {"ip", client_ip},
+        {"URI", request.target()},
+        {"method", http::to_string(request.method())}
+    };
+    BOOST_LOG_TRIVIAL(info) << logging::add_value(log_data, data) << "request received";
+}
+
+template <typename Response>
+void LogResponse(long long time, Response& response) {
+    auto content_type = response[http::field::content_type];
+    if (content_type.empty()) {
+        content_type = "null";
+    }
+    boost::json::value data = {
+        {"response_time", time},
+        {"code", response.result_int()},
+        {"content_type", content_type}
+    };
+    BOOST_LOG_TRIVIAL(info) << logging::add_value(log_data, data) << "response sent";
+}
+
+
+void LogServerStart(unsigned int port, std::string_view address);
+void LogServerEnd(unsigned int code, std::string_view exeption_text);
+void LogServerError(unsigned int code, std::string_view message, std::string_view where);
+} // namespace http_logger
---
+++ sprint2/problems/time_control/solution/src/model.cpp
@@ -0,0 +1,381 @@
+#include "model.h"
+
+#include <algorithm>
+#include <cmath>
+#include <random>
+#include <stdexcept>
+
+namespace model {
+using namespace std::literals;
+
+std::string DirectionToString(Direction dir) {
+    switch (dir) {
+        case Direction::NORTH:
+            return "U"s;
+        case Direction::SOUTH:
+            return "D"s;
+        case Direction::EAST:
+            return "R"s;
+        case Direction::WEST:
+            return "L"s;
+    }
+    throw std::runtime_error("Unknown direction status in Dog class"s);
+}
+
+Road::Road(HorizontalTag, Point start, Coord end_x) noexcept
+    : start_{start}
+    , end_{end_x, start.y} {
+}
+
+Road::Road(VerticalTag, Point start, Coord end_y) noexcept
+    : start_{start}
+    , end_{start.x, end_y} {
+}
+
+bool Road::IsHorizontal() const noexcept {
+    return start_.y == end_.y;
+}
+
+bool Road::IsVertical() const noexcept {
+    return start_.x == end_.x;
+}
+
+Point Road::GetStart() const noexcept {
+    return start_;
+}
+
+Point Road::GetEnd() const noexcept {
+    return end_;
+}
+
+bool Road::IsPointOnRoad(Point point) const {
+    if (IsVertical()) {
+        return start_.x == point.x
+            && point.y >= std::min(start_.y, end_.y)
+            && point.y <= std::max(start_.y, end_.y);
+    } else {
+        return start_.y == point.y
+            && point.x >= std::min(start_.x, end_.x)
+            && point.x <= std::max(start_.x, end_.x);
+    }
+}
+
+const Rectangle& Building::GetBounds() const noexcept {
+    return bounds_;
+}
+
+const Office::Id& Office::GetId() const noexcept {
+    return id_;
+}
+
+Point Office::GetPosition() const noexcept {
+    return position_;
+}
+
+Offset Office::GetOffset() const noexcept {
+    return offset_;
+}
+
+const Map::Id& Map::GetId() const noexcept {
+    return id_;
+}
+
+const std::string& Map::GetName() const noexcept {
+    return name_;
+}
+
+Velocity Map::GetSpeed() const noexcept {
+    return dog_speed_;
+}
+
+const Map::Buildings& Map::GetBuildings() const noexcept {
+    return buildings_;
+}
+
+const Map::Roads& Map::GetRoads() const noexcept {
+    return roads_;
+}
+
+const Map::Offices& Map::GetOffices() const noexcept {
+    return offices_;
+}
+
+void Map::SetDogSpeed(Velocity speed) {
+    dog_speed_ = speed;
+}
+
+void Map::AddRoad(Road&& road) {
+    roads_.emplace_back(std::move(road));
+    auto& new_road = roads_.back();
+    if (new_road.IsVertical()) {
+        vertical_road_index_[new_road.GetStart().x].push_back(&new_road);
+    } else {
+        horizontal_road_index_[new_road.GetStart().y].push_back(&new_road);
+    }
+}
+
+void Map::AddBuilding(Building&& building) {
+    buildings_.emplace_back(std::move(building));
+}
+
+void Map::AddOffice(Office&& office) {
+    if (warehouse_id_to_index_.contains(office.GetId())) {
+        throw std::invalid_argument("Duplicate warehouse");
+    }
+
+    const size_t index = offices_.size();
+    Office& o = offices_.emplace_back(std::move(office));
+    try {
+        warehouse_id_to_index_.emplace(o.GetId(), index);
+    } catch (...) {
+        // Удаляем офис из вектора, если не удалось вставить в unordered_map
+        offices_.pop_back();
+        throw;
+    }
+}
+
+DogPoint Map::GetRandomDogPoint() const {
+    if (roads_.size() == 0) {
+        throw std::logic_error("No roads on map to generate random road"s);
+    }
+
+    std::random_device random_device;
+    std::mt19937 generator{random_device()};
+
+    std::uniform_int_distribution int_dist(0, static_cast<int>(roads_.size() - 1));
+
+    const Road& rand_road = roads_.at(int_dist(generator));
+
+    if (rand_road.IsHorizontal()) {
+        std::uniform_real_distribution double_dist(std::min(rand_road.GetStart().x, rand_road.GetEnd().x) + 0.0,
+                                                   std::max(rand_road.GetStart().x, rand_road.GetEnd().x) + 0.0);
+        return {static_cast<DogCoord>(double_dist(generator)), static_cast<DogCoord>(rand_road.GetStart().y)};
+    } else {
+        std::uniform_real_distribution double_dist(std::min(rand_road.GetStart().y, rand_road.GetEnd().y) + 0.0,
+                                                   std::max(rand_road.GetStart().y, rand_road.GetEnd().y) + 0.0);
+        return {static_cast<DogCoord>(rand_road.GetStart().x), static_cast<DogCoord>(double_dist(generator))};
+    }
+}
+
+const Road* Map::GetVerticalRoad(DogPoint dog_point) const {
+    Point map_point = dog_point.ConvertToMapPoint();
+
+    double difference_x = (double)map_point.x - dog_point.x;
+    double difference_y = (double)map_point.y- dog_point.y;
+
+    if (difference_x > Road::WIDTH_ROAD_COEF_MINUS
+        && difference_x < Road::WIDTH_ROAD_COEF_PLUS
+        && vertical_road_index_.contains(map_point.x)) {
+        for (const Road* road : vertical_road_index_.at(map_point.x)) {
+            if ((road->GetStart().y == map_point.y || road->GetEnd().y == map_point.y)
+                && difference_y > Road::WIDTH_ROAD_COEF_MINUS
+                && difference_y < Road::WIDTH_ROAD_COEF_PLUS) {
+                continue;
+            }
+
+            if (road->IsPointOnRoad(map_point)) {
+                return road;
+            }
+        }
+    }
+    return nullptr;
+}
+
+const Road* Map::GetHorizontalRoad(DogPoint dog_point) const {
+    Point map_point = dog_point.ConvertToMapPoint();
+
+    double difference_x = (double)map_point.x - dog_point.x;
+    double difference_y = (double)map_point.y- dog_point.y;
+
+    if (difference_y > Road::WIDTH_ROAD_COEF_MINUS
+        && difference_y < Road::WIDTH_ROAD_COEF_PLUS
+        && vertical_road_index_.contains(map_point.y)) {
+        for (const Road* road : horizontal_road_index_.at(map_point.y)) {
+            if ((road->GetStart().x == map_point.x || road->GetEnd().x == map_point.x)
+                && difference_x > Road::WIDTH_ROAD_COEF_MINUS
+                && difference_x < Road::WIDTH_ROAD_COEF_PLUS) {
+                continue;
+            }
+
+            if (road->IsPointOnRoad(map_point)) {
+                return road;
+            }
+        }
+    }
+    return nullptr;
+}
+
+const std::string& Dog::GetName() const noexcept {
+    return name_;
+}
+
+const Dog::Id& Dog::GetId() const noexcept {
+    return id_;
+}
+
+void Dog::SetName(std::string_view name) {
+    name_ = std::string(name);
+}
+
+void Dog::SetPosition(DogPoint new_pos) {
+    pos_ = new_pos;
+}
+
+const DogPoint& Dog::GetPosition() const {
+    return pos_;
+}
+
+void Dog::SetSpeed(Speed new_speed) {
+    speed_ = new_speed;
+}
+
+const Speed& Dog::GetSpeed() const {
+    return speed_;
+}
+
+void Dog::SetDirection(Direction new_dir) {
+    dir_ = new_dir;
+}
+
+Direction Dog::GetDirection() const {
+    return dir_;
+
+}
+
+void Dog::Stop() {
+    speed_ = {0, 0};
+}
+
+bool Dog::IsStopped() const {
+    return std::fabs(speed_.s_x) < std::numeric_limits<double>::epsilon()
+        && std::fabs(speed_.s_y) < std::numeric_limits<double>::epsilon();
+}
+
+const Map::Id& GameSession::GetMapId() const {
+    assert(map_ != nullptr);
+    return map_->GetId();
+}
+
+const model::Map* GameSession::GetMap() const {
+    return map_;
+}
+
+Dog* GameSession::AddDog(std::string_view name) {
+    detail::ThreadChecker checker(counter_);
+
+    Speed default_speed = {0, 0};
+    model::DogPoint dog_pos = map_->GetRandomDogPoint();
+
+    auto dog = std::make_shared<Dog>(std::string(name), dog_pos, default_speed);
+    auto dog_id = dog->GetId();
+    dogs_.emplace(dog_id, std::move(dog));
+    return dogs_.at(dog_id).get();
+}
+
+const GameSession::IdToDogIndex& GameSession::GetDogs() const {
+    return dogs_;
+}
+
+void GameSession::UpdateState(std::uint64_t tick) {
+    double ms_convertion = 0.001; // 1ms = 0.001s
+    double tick_multy = tick * ms_convertion;
+
+    for (auto& [_, dog] : dogs_) {
+        if (dog->IsStopped()) {
+            continue;;
+        }
+
+        auto cur_dog_pos = dog->GetPosition();
+        auto dog_speed = dog->GetSpeed();
+        DogPoint new_dog_pos = {cur_dog_pos.x + (dog_speed.s_x * tick_multy),
+                            cur_dog_pos.y + (dog_speed.s_y * tick_multy)};
+
+        const Road* vertical_road_with_dog = map_->GetVerticalRoad(dog->GetPosition());
+        const Road* horizontal_road_with_dog = map_->GetHorizontalRoad(dog->GetPosition());
+
+        const Road* new_vertical_road_with_dog = map_->GetVerticalRoad(new_dog_pos);
+        const Road* new_horizontal_road_with_dog = map_->GetHorizontalRoad(new_dog_pos);
+
+        if (new_vertical_road_with_dog != vertical_road_with_dog
+            || new_horizontal_road_with_dog != horizontal_road_with_dog) {
+            
+            Point cur_dog_pos_on_map = cur_dog_pos.ConvertToMapPoint();
+
+            switch (dog->GetDirection()) {
+                case Direction::NORTH:
+                    new_dog_pos = {new_dog_pos.x, cur_dog_pos_on_map.y - 0.4};
+                    break;
+                case Direction::SOUTH:
+                    new_dog_pos = {new_dog_pos.x, cur_dog_pos_on_map.y + 0.4};
+                    break;
+                case Direction::WEST:
+                    new_dog_pos = {cur_dog_pos_on_map.x - 0.4, new_dog_pos.y};
+                    break;
+                case Direction::EAST:
+                    new_dog_pos = {cur_dog_pos_on_map.x + 0.4, new_dog_pos.y};
+                    break;
+
+                default:
+                    throw std::runtime_error("unknown direction for dog");
+            }
+            dog->Stop();
+        }
+        dog->SetPosition(new_dog_pos);
+    }
+}
+
+void Game::AddMap(Map map) {
+    const size_t index = maps_.size();
+    if (auto [it, inserted] = map_id_to_index_.emplace(map.GetId(), index); !inserted) {
+        throw std::invalid_argument("Map with id "s + *map.GetId() + " already exists"s);
+    } else {
+        try {
+            maps_.emplace_back(std::move(map));
+        } catch (...) {
+            map_id_to_index_.erase(it);
+            throw;
+        }
+    }
+}
+
+const Game::Maps& Game::GetMaps() const noexcept {
+    return maps_;
+}
+
+const Map* Game::FindMap(const Map::Id& id) const noexcept {
+    if (auto it = map_id_to_index_.find(id); it != map_id_to_index_.end()) {
+        return &maps_.at(it->second);
+    }
+    return nullptr;
+}
+
+GameSession& Game::StartGameSession(const Map* map) {
+    if (sessions_[map->GetId()].empty()) {
+        sessions_[map->GetId()].push_back(std::make_unique<GameSession>(map));
+    }
+    return *sessions_[map->GetId()].back();
+}
+
+GameSession* Game::GetGameSession(Map::Id map_id) {
+    if (sessions_[map_id].empty()) {
+        return nullptr;
+    }
+    return sessions_[map_id].back().get();
+}
+
+void Game::SetDogSpeed(Velocity default_speed) {
+    default_dog_speed_ = default_speed;
+}
+
+Velocity Game::GetDefaultGogSpeed() const noexcept {
+    return default_dog_speed_;
+}
+
+void Game::UpdateState(std::uint64_t tick) {
+    for (auto& [_, map_sessions] : sessions_) {
+        for (auto& session : map_sessions) {
+            session->UpdateState(tick);
+        }
+    }
+}
+}  // namespace model
