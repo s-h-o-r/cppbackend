7a3a85088fe03258adfb2ebcec878deb33e29951
Shor <danilbug@mail.ru>
2024-11-02 21:16:12 +0300

    gather-test solution (added new test for diagonals movments) v4

--- sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
+++ sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
@@ -13,6 +13,8 @@
 
 using Catch::Matchers::Contains;
 using Catch::Matchers::WithinRel;
+using Catch::Matchers::WithinAbs;
+
 using namespace collision_detector;
 using namespace std::literals;
 
@@ -101,18 +103,21 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
     REQUIRE(FindGatherEvents(provider).size() == 0);
 
     std::vector<GatheringEvent> right_events;
+    CollectionResult buffer_result;
     SECTION("Gatherer moves through the items") {
 
         SECTION("Gatherer moves vertical") {
             provider.AddGatherer({{1., 1.}, {1., 2.}, 1.})
                     .AddItem({{1., 1.5}, 1.});
-            right_events.push_back({0, 0, 0., 0.});
+            buffer_result = TryCollectPoint({1., 1.}, {1., 2.}, {1., 1.5});
+            right_events.push_back({0, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
             SECTION("Gatherer takes one item in the middle of the route") {
                 CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
             }
 
             provider.AddItem({{1., 2.}, 1.});
-            right_events.push_back({1, 0, 0., 0.});
+            buffer_result = TryCollectPoint({1., 1.}, {1., 2.}, {1., 2.});
+            right_events.push_back({1, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
             SECTION("Gatherer takes two items: in the middle of the route and in the end") {
                 CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
             }
@@ -120,8 +125,11 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
             provider.AddItem({{3., 2.}, 1.})
                     .AddItem({{-1., 2.}, 1.});
 
-            right_events.push_back({2, 0, 0., 0.});
-            right_events.push_back({3, 0, 0., 0.});
+            buffer_result = TryCollectPoint({1., 1.}, {1., 2.}, {3., 2.});
+            right_events.push_back({2, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
+
+            buffer_result = TryCollectPoint({1., 1.}, {1., 2.}, {-1., 2.});
+            right_events.push_back({3, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
             SECTION("Gatherer must collect items on the edges") {
                 CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
             }
@@ -140,6 +148,10 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
 
             auto events = FindGatherEvents(provider);
             SECTION("events follow in chronological order") {
+                INFO("added new gatherer which gathers an item before first gatherer");
+                provider.AddGatherer({{5., 5.}, {10., 5}, 1.})
+                    .AddItem({{5.5, 5.}, 1.});
+
                 CHECK(std::is_sorted(events.begin(), events.end(), [](const GatheringEvent& lhs,
                                                                       const GatheringEvent& rhs) {
                     return lhs.time < rhs.time;
@@ -151,11 +163,11 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
                     Item item = provider.GetItem(event.item_id);
                     Gatherer gatherer = provider.GetGatherer(event.gatherer_id);
 
-                    auto [sq_distance, proj_ratio] = TryCollectPoint(gatherer.start_pos, gatherer.end_pos,
+                    buffer_result = TryCollectPoint(gatherer.start_pos, gatherer.end_pos,
                                                                      item.position);
 
-                    REQUIRE_THAT(event.sq_distance, WithinRel(sq_distance, 1e-10));
-                    REQUIRE_THAT(event.time, WithinRel(proj_ratio, 1e-10));
+                    REQUIRE_THAT(event.sq_distance, WithinRel(buffer_result.sq_distance, 1e-10));
+                    REQUIRE_THAT(event.time, WithinRel(buffer_result.proj_ratio, 1e-10));
                 }
             }
         }
@@ -163,13 +175,16 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
         SECTION("Gatherer moves horizontal") {
             provider.AddGatherer({{1., 1.}, {2., 1.}, 1.})
                     .AddItem({{1.5, 1.}, 1.});
-            right_events.push_back({0, 0, 0., 0.});
+
+            buffer_result = TryCollectPoint({1., 1.}, {2., 1.}, {1.5, 1.});
+            right_events.push_back({0, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
             SECTION("Gatherer takes one item in the middle of the route") {
                 CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
             }
 
             provider.AddItem({{2., 1.}, 1.});
-            right_events.push_back({1, 0, 0., 0.});
+            buffer_result = TryCollectPoint({1., 1.}, {2., 1.}, {2., 1.});
+            right_events.push_back({1, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
             SECTION("Gatherer takes two items: in the middle of the route and in the end") {
                 CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
             }
@@ -177,8 +192,10 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
             provider.AddItem({{2., 3.}, 1.})
                     .AddItem({{2., -1.}, 1.});
 
-            right_events.push_back({2, 0, 0., 0.});
-            right_events.push_back({3, 0, 0., 0.});
+            buffer_result = TryCollectPoint({1., 1.}, {2., 1.}, {2., 3.});
+            right_events.push_back({2, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
+            buffer_result = TryCollectPoint({1., 1.}, {2., 1.}, {2., -1.});
+            right_events.push_back({3, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
             SECTION("Gatherer must collect items on the edges") {
                 CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
             }
@@ -203,6 +220,7 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
             }
 
             SECTION("FindGatherEvents returns right data (sq_distance and time)") {
+                provider.AddItem({{3., 1.}, 1.});
                 for (const auto& event : events) {
                     Item item = provider.GetItem(event.item_id);
                     Gatherer gatherer = provider.GetGatherer(event.gatherer_id);
@@ -210,10 +228,21 @@ TEST_CASE("FindGatherEvents test case", "[gather events]") {
                     auto [sq_distance, proj_ratio] = TryCollectPoint(gatherer.start_pos, gatherer.end_pos,
                                                                      item.position);
 
-                    REQUIRE_THAT(event.sq_distance, WithinRel(sq_distance, 1e-10));
-                    REQUIRE_THAT(event.time, WithinRel(proj_ratio, 1e-10));
+                    REQUIRE_THAT(event.sq_distance, WithinAbs(sq_distance, 1e-10));
+                    REQUIRE_THAT(event.time, WithinAbs(proj_ratio, 1e-10));
                 }
             }
+        }
+
+        SECTION("Gatherer moves diagonal") {
+            provider.AddGatherer({{10., 10.}, {5., 5.}, 1.})
+                    .AddItem({{6., 6.}, 1.});
+            buffer_result = TryCollectPoint({10., 10.}, {5., 5.}, {6., 6.});
+            right_events.push_back({0, 0, buffer_result.sq_distance, buffer_result.proj_ratio});
+            SECTION("Gatherer must take the item") {
+                CHECK_THAT(FindGatherEvents(provider), IsPermutation(right_events));
+            }
+
         }
     }
 }
