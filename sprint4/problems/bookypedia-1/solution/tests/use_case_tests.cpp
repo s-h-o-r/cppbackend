a5325849c67adf24b77f7ee6e7420ae71165ffaa
Shor <danilbug@mail.ru>
2024-11-30 14:33:14 +0300

    fixed repositories tests

--- sprint4/problems/bookypedia-1/solution/tests/use_case_tests.cpp
+++ sprint4/problems/bookypedia-1/solution/tests/use_case_tests.cpp
@@ -2,6 +2,10 @@
 
 #include "../src/app/use_cases_impl.h"
 #include "../src/domain/author.h"
+#include "../src/domain/book.h"
+
+#include <algorithm>
+#include <vector>
 
 namespace {
 
@@ -11,17 +15,45 @@ struct MockAuthorRepository : domain::AuthorRepository {
     void Save(const domain::Author& author) override {
         saved_authors.emplace_back(author);
     }
+
+    std::vector<domain::Author> GetAuthors() override {
+        return saved_authors;
+    }
+};
+
+struct MockBookRepository : domain::BookRepository {
+    std::vector<domain::Book> saved_books;
+
+    void Save(const domain::Book& book) override {
+        saved_books.emplace_back(book);
+    }
+
+    std::vector<domain::Book> GetAllBooks() override {
+        return saved_books;
+    }
+
+    std::vector<domain::Book> GetAuthorBooks(const domain::AuthorId& author_id) override {
+        std::vector<domain::Book> author_books;
+
+        for (const auto& book : saved_books) {
+            if (book.GetAuthorId() == author_id) {
+                author_books.push_back(book);
+            }
+        }
+        return author_books;
+    }
 };
 
 struct Fixture {
     MockAuthorRepository authors;
+    MockBookRepository books;
 };
 
 }  // namespace
 
 SCENARIO_METHOD(Fixture, "Book Adding") {
     GIVEN("Use cases") {
-        app::UseCasesImpl use_cases{authors};
+        app::UseCasesImpl use_cases{authors, books};
 
         WHEN("Adding an author") {
             const auto author_name = "Joanne Rowling";
